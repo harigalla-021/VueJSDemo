2b5cd0388434ba17d0695b56e0941d4b2ea696bb
Hari Galla <hari.galla@capgemini.com>
2020-04-26 15:10:18 +0530

    commited again

---
+++ src/StringBufferDemo.java
@@ -0,0 +1,18 @@
+
+public class StringBufferDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1=new String("Hari");
+		String s2=new String("Galla");
+		StringBuffer sb1=new StringBuffer("Hello");
+		StringBuffer sb2=new StringBuffer("World");
+		sb1.append(sb2);
+
+	}
+
+}
